b594a1985a9e01c268ff908e883af0635cfc757a
Adam Lloyd <adam@alloy-d.net>
2011-10-17 02:05:14 -0400

    Split Gists and Gist classes into separate files.

--- lib/gist.rb
+++ lib/gist.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
-require 'net/http'
-require 'json'
+require 'gists'
 
 class Gist
   attr_accessor :id, :description, :public, :files
@@ -49,49 +48,3 @@ class Gist
   end
 end
 
-class Gists
-  API_ROOT = "api.github.com"
-  def initialize(username, password)
-    @username = username
-    @password = password
-  end
-
-  def get(resource)
-    Net::HTTP.start(API_ROOT, :use_ssl => true) do |http|
-      req = Net::HTTP::Get.new(resource)
-      req.basic_auth @username, @password
-      response = http.request(req)
-      
-      JSON.parse(response.body, :symbolize_names => true)
-    end
-  end
-
-  def post(resource, body)
-    Net::HTTP.start(API_ROOT, :use_ssl => true) do |http|
-      req = Net::HTTP::Post.new(resource)
-      req.basic_auth @username, @password
-      req.body = body
-      response = http.request(req)
-
-      JSON.parse(response.body, :symbolize_names => true)
-    end
-  end
-
-  def get_all
-    get("/gists")
-  end
-
-  def get_gist(id)
-    Gist.json_create(get("/gists/#{id}"))
-  end
-
-  def save(gist)
-    if not gist.id.nil? then
-      post "/gists/#{gist.id}", gist.to_json
-    else
-      res = post "/gists", gist.to_json
-      gist.id = res["id"]
-      res
-    end
-  end
-end
